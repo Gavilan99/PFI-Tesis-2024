134045b916d084ec8b372a2475f6f2ce6b09b77e
Gavilan99 <jmgavilan99@gmail.com>
2024-08-30 14:49:32 -0300

    Demo V2
    Added questions and a system to pick them at random.

--- scr/demo.py
+++ scr/demo.py
@@ -1,7 +1,7 @@
 import joblib
 import os
+import random
 import pandas as pd
-
 
 #search path
 search_path = os.path.join('PFI-Tesis-2024', 'models', 'decision_tree_model.pkl') #get the correct path for the DTC
@@ -9,12 +9,41 @@ search_path = os.path.join('PFI-Tesis-2024', 'models', 'decision_tree_model.pkl'
 #load the DTC to make the prediction
 dtc = joblib.load(search_path)
 
+#dictionay with questions, a dictionary per group. Questions : List of answers
+hor_questions = {
+    "En una situación en la que tus ideas son cuestionadas, ¿cómo respondes habitualmente?" : ["Afirmo mi punto de vista y mantengo mi postura","Intento conciliar las diferencias y mantener la armonía","Tomo distancia para reflexionar y reconsiderar mi posición"],
+    "Si te sientes abrumado, ¿cuál es tu respuesta típica?" : ["Sigo adelante y tomo acción","Me retiro para reflexionar y recargar mi energía","Me retiro para reflexionar y recargar mi energía"],
+}
+harc_questions = {
+    "Imagina que te enfrentas con un obstáculo en tu camino hacia un objetivo, ¿cómo reaccionarías?" : ["Me concentro en encontrar una solución y seguir adelante","Mantengo una actitud positiva y busco el lado bueno", "Reacciono emocionalmente y expreso mi frustración"],
+    "¿Cómo afrontas los problemas inesperados que interrumpen tus planes?" : ["Me concentro en resolver el problema con determinación", "Intento mantenerme optimista y espero encontrar una solución", "Manifiesto mis opiniones con convicción"]
+}
+hary_questions= {
+    "Cuando trabajas en un proyecto grupal, ¿qué es lo que más motiva tu comportamiento?" : ["Asegurarme de que todos se sientan involucrados y que el proyecto refleje el consenso del grupo", "Asegurarme de que el proyecto cumpla con un alto estándar de excelencia, incluso si eso implica hacer críticas", "Asegurarme de que el proyecto se mantenga encaminado tomando la iniciativa, incluso si eso implica tomar decisiones difíciles"],
+    "¿Qué te motiva a ayudar a otros?" : ["El deseo de ser una parte valiosa e integral del grupo.", "La frustración de ver a otros luchar y querer mejorar su situación", "La necesidad de asegurar mi importancia en la vida de otros y que los demás puedan contar conmigo"]
+}
+triad_questions ={
+    "Cuando alguien te critica, ¿cuál es tu reacción inicial?" : ["Me siento herido o rechazado","Me defiendo instintivamente","Analizo la validez de la crítica"],
+    "¿Cómo aborda la resolución de problemas en una situación de alta presión?" : ["Confío en mis emociones y en cómo me hace sentir la situación","Confío en mis instintos para guiarme","Pienso en la situación lógicamente"]
+}
+
+def getRandomKey(dct):
+    """Gets a random key from a dictionary"""
+    num = random.randint(0,len(dct))
+    aux = list(dct.keys())
+    return aux[num]
+
+
 #questions and answers for the user to give
+hor_index = getRandomKey(hor_questions)
+harc_index = getRandomKey(harc_questions)
+hary_index = getRandomKey(hary_questions)
+triad_index = getRandomKey(triad_questions)
 print("Seleccione una opción para cada pregunta:")
-hornevian = input("1. Hornevian: \n1) Assertive \n2) Compliant \n3) Withdrawn\n")
-harmonic = input("2.  Harmonic: \n1) Positive \n2) Reactive \n3) Competency\n")
-harmony = input("3. Harmony: \n1) Attachment \n2) Frustration \n3) Rejection\n")
-triad = input("4. Triad: \n1) Feeling \n2) Intuition \n3) Thought\n")
+hornevian = input("P: {question}: \n1){assertive} (assertive) \n2){compliant} (compliant) \n3) {withdrawn} (withdrawn)\nSeleccione una opción: ".format(question = hor_index, assertive = hor_questions[hor_index][0], compliant = hor_questions[hor_index][1], withdrawn = hor_questions[hor_index][2]))
+harmonic = input("P: {question}: \n1){competency} (competency) \n2){positive} (positive) \n3) {reactive} (reactive) \nSeleccione una opción: ".format(question = harc_index, competency = harc_questions[harc_index][0], positive = harc_questions[harc_index][1], reactive = harc_questions[harc_index][2]))
+harmony = input("P: {question}: \n1){attachment} (attachment) \n2){frustration} (frustration) \n3) {rejection} (rejection) \nSeleccione una opción: ".format(question = hary_index, attachment = hary_questions[hary_index][0], frustration = hary_questions[hary_index][1], rejection = hary_questions[hary_index][2]))
+triad = input("P: {question}: \n1){feeling} (feeling) \n2){intuition} (intuition) \n3) {thought} (thought) \nSeleccione una opción: ".format(question =triad_index, feeling = triad_questions[triad_index][0], intuition = triad_questions[triad_index][1], thought = triad_questions[triad_index][2]))
 
 #map the answers
 encoded_responses = {
